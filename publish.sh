236f6973e8d0c403d3b69c3bfa1d21ed3ced022e
Joshua Powers <josh.powers@canonical.com>
2018-01-18 15:43:11 -0800

    public script

--- publish.sh
+++ publish.sh
@@ -1,35 +1,30 @@
 #!/bin/bash
 set -ux
 
-# Generate a new temporary directory for our deployment
-deploy_dir=$(mktemp -d -t hugo-deploy)
+deploy_dir="public"
+rm -rf "$deploy_dir"
 echo "Using deploy directory ${deploy_dir}"
 
-# Determine the remote origin Git location
 git_remote=$(git remote get-url --push origin)
 echo "Determined that Git remote is ${git_remote}"
 
-# Update git submodules
 echo "Updating Git submodules"
 git submodule init
 git submodule update
 
-# Build the site in the deploy directory
-echo
 echo "Building Hugo site"
-hugo -d "$deploy_dir"
+hugo --destination "$deploy_dir"
 
-# Initialise the deploy directory as a Git repository and add content
-cd "$deploy_dir"
-
-echo
 echo "Creating a new Git repository and adding content"
+cd "$deploy_dir"
 git init
 git remote add origin "$git_remote"
 git checkout --orphan master
 git add .
 git commit -m "Site updated at $(date -u "+%Y-%m-%d %H:%M:%S") UTC"
 
-echo
 echo "Deploying code to the master branch"
-#git push --force origin master
+git push --force origin master
+
+rm -rf "$deploy_dir"
+
